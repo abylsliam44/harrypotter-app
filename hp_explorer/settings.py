3af246071313b41cd012fd56018d0efd180f1da0
abylsliam44 <abylajslamzanov@gmail.com>
2025-04-26 19:09:41 -0400

    update settings.py

--- hp_explorer/settings.py
+++ hp_explorer/settings.py
@@ -81,14 +81,13 @@ WSGI_APPLICATION = 'hp_explorer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hp_db',           
-        'USER': 'postgres',
-        'PASSWORD': 'admin',  
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),         
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),  
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
